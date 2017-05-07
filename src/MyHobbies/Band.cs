de8f33c75d667823a23fffbf0e598c886f00cba8
Riwan Carpentier <riwan.carpentier@student.howest.be>
2017-05-07 21:06:18 +0200

    oefening 2

---
+++ src/MyHobbies/Band.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHobbies
+{
+    public class Band
+    {
+        public string Naam { get; set; }
+        public int Jaar { get; set; }
+        public List<Bandlid> Bandleden { get; set; }
+    }
+}
---
+++ src/MyHobbies/Bandlid.cs
@@ -0,0 +1,16 @@
+﻿namespace MyHobbies
+{
+    public enum Geslacht
+    {
+        Man,
+        Vrouw
+    }
+
+    public class Bandlid
+    {
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public Geslacht Geslacht { get; set; }
+        public bool Levend { get; set; }
+    }
+}
